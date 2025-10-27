80b484ad0a9d8edf72970c6da07cc1673cf37ee9
Horrar03 <luisfernandosernalopez@gmail.com>
2025-10-27 16:31:19 -0600

    twosum

---
+++ twosum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const seen = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (seen.hasOwnProperty(complement)) {
+            return [seen[complement], i];
+        }
+        seen[nums[i]] = i;
+    }
+
+    return [];
+};
