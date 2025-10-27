d5da446d3a1832f0507d978baa00f44bba42c4a7
Horrar03 <luisfernandosernalopez@gmail.com>
2025-10-27 16:14:50 -0600

    Commit inicial

---
+++ counter.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++
+    };
+};
+
+
+const counter = createCounter(3);
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
